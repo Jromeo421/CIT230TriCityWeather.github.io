b51d69d42f7a3f5956c3bc74fcc06ef83b361dd4
Jromeo421 <hug17010@byui.edu>
2018-07-02 03:12:34 -0500

    active pages

--- assignments/weathersite/scripts/active.js
+++ assignments/weathersite/scripts/active.js
@@ -1,13 +1,15 @@
 // This changes the active tag to the current page
-var urlString, urlArray, pageHREF, menu, i, currentURL;
-urlString = document.location.href;
-urlArray = urlString.split('/');
-pageHREF = urlArray[urlArray.length - 1];
+var urlString = document.location.href;
+var urlArray = urlString.split('/');
+var pageHREF = urlArray[urlArray.length - 1];
 
 if (pageHREF !== "") {
-    menu = document.querySelectorAll('ul#menuNav li a');
+   var menu = document.querySelectorAll('ul#menuNav li a');
+    
+  var i;
+    
     for (i = 0; i < menu.length; i++) {
-        currentURL = (menu[i].getAttribute('href'));
+        var currentURL = (menu[i].getAttribute('href'));
         menu[i].parentNode.className = '';
         if (currentURL === pageHREF) {
             menu[i].parentNode.className = 'active';
